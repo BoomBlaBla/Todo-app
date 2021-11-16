3c6962e2397367c91540f7903f60ce63f21f5f65
BoomBlaBla <877475462@qq.com>
2021-11-17 00:55:42 +0800

    修复表单数据绑定

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,4 +1,4 @@
-import { List } from '@arco-design/web-react';
+import { List , Input } from '@arco-design/web-react';
 import React, {Component} from 'react';
 import './ToDoItem.css';
 import {CheckRadio} from './CheckRadio';
@@ -8,52 +8,73 @@ class ToDoItem extends Component{
     constructor(props){
         super(props);
         this.state = {
-            finished:props.finished,
-            important:props.important,
+            editing:false,
             content:props.content,
-            steps:props.steps,
-            current:0,
-            deadline:props.deadline,
         }
         this.handleRadioClick = this.handleRadioClick.bind(this);
         this.handleStarClick = this.handleStarClick.bind(this);
         this.handleItemClick = this.handleItemClick.bind(this);
+        this.updateContent = this.updateContent.bind(this);
+        this.updateContentAfterEnter = this.updateContentAfterEnter.bind(this);
+        this.inputRef = React.createRef();
     }
 
     handleRadioClick(e){
-        this.setState({finished : !this.state.finished});
+        this.props.onFinishedRadioClick(this.props.index);
         e.stopPropagation();
     }
 
     handleStarClick(e){
-        this.setState({important : !this.state.important});
+        this.props.onStarRadioClick(this.props.index);
         e.stopPropagation();
     }
 
     handleItemClick(){
-        this.props.onClick(this.props);
+        if(this.props.onClick) this.props.onClick(this.props.index);
+    }
+
+    updateContent(){
+        this.props.onContentChange(this.state.content);
+    }
+
+    updateContentAfterEnter(){
+        this.updateContent();
+        this.inputRef.blur();
     }
 
     render(){
         const props = this.props;
-        const state = this.state ; 
+        const TitleElement = this.props.editable?
+                                (<Input ref={(ref)=>{this.inputRef = ref}} 
+                                    className="no-border"
+                                    style={{textDecoration:props.finished&&!props.editing?'line-through':'' , backgroundColor:'white'}} 
+                                    defaultValue={props.content} 
+                                    onChange={(val)=>{this.setState({content:val})}}
+                                    onPressEnter={this.updateContentAfterEnter}
+                                    onBlur={this.updateContent}
+                                    />):
+                                (<span style={{textDecoration:props.finished?'line-through':''}}>
+                                    {props.content}
+                                </span>);
+        const unfinishedStepIndexs = this.props.steps==null || this.props.steps.find((step,index)=>{
+            if(!step.finished){
+                return index;
+            }
+        });
+        const current = (unfinishedStepIndexs==null?1:(unfinishedStepIndexs[0]+1));
         return (
             <List.Item key={props.id} className="list-item-normal"
-                style={this.props.style}
+                style={props.style}
                 onClick={this.handleItemClick}
                 extra={
-                    <StarRadio fillColor={this.props.fillColor} className="star-button" important={state.important} onClick={this.handleStarClick}/>
+                    <StarRadio fillColor={props.fillColor} className="star-button" important={props.important} onClick={this.handleStarClick}/>
                 }>
                 <List.Item.Meta
                     avatar={
-                        <CheckRadio fillColor={this.props.fillColor} checked={state.finished} onClick={this.handleRadioClick}/> 
+                        <CheckRadio fillColor={props.fillColor} checked={props.finished} onClick={this.handleRadioClick}/> 
                     }
-                    title={
-                        <span style={{textDecoration:state.finished?'line-through':''}}>
-                            {props.content}
-                        </span>
-                        }
-                    description={props.steps!=null && props.steps.length > 0 && "任务#"+props.steps[0] + ' . 第'+state.current+'步 , 共'+state.steps.length+'步'}
+                    title={TitleElement}
+                    description={props.steps!=null && props.steps.length > 0 && "任务#"+props.steps[0] + ' . 第'+current+'步 , 共'+props.steps.length+'步'}
                 />
             </List.Item>
         );
